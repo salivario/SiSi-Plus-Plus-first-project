986c64fef61f0f4fcef2d6ad8724f1bb009b369e
Mihail <dghut120@gmail.com>
2024-12-23 09:59:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+    int x;
+    int y;
+    char operand;
+    std::cout << "введите число x: ";
+
+    if (!(std::cin >> x)) {
+        std::cout << "введите ЧИСЛО x: ";
+    }
+    else {
+        std::cout << "введите число y: ";
+        if (!(std::cin >> y)) {
+            std::cout << "введите ЧИСЛО y: ";
+        }
+        else
+        {
+            std::cout << "введите операнд: ";
+            if (!(std::cin >> operand) 
+                && (operand != "-")
+                && (operand != "+")
+                && (operand != "*")
+                && (operand != "/")) {
+                std::cout << "введите ОПЕРАНД: ";
+            }
+            else {
+                std::cout << "Вы ввели: " << x << operand << y << std::endl;
+            }
+        }
+    }
+}
